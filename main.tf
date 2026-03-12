d39fd4034a7eceef29613f0ae09f9be96428443a
arudra sai sivaraj <saiarudra7@gmail.com>
2026-03-12 18:50:37 +0530

    created nodes for cluster

--- main.tf
+++ main.tf
@@ -306,3 +306,27 @@ resource "aws_eks_cluster" "sai_eks" {
     aws_iam_role_policy_attachment.eks_cluster_policy
   ]
 }
+resource "aws_eks_node_group" "sai_nodes" {
+  cluster_name    = aws_eks_cluster.sai_eks.name
+  node_group_name = "sai-node-group"
+  node_role_arn   = aws_iam_role.eks_node_role.arn
+
+  subnet_ids = [
+    aws_subnet.private_subnet_1.id,
+    aws_subnet.private_subnet_2.id
+  ]
+
+  instance_types = ["t3.medium"]
+
+  scaling_config {
+    desired_size = 2
+    max_size     = 3
+    min_size     = 1
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_worker_node_policy,
+    aws_iam_role_policy_attachment.eks_cni_policy,
+    aws_iam_role_policy_attachment.eks_registry_policy
+  ]
+}
